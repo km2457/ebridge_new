bc705b00e01fbb22e6702cffc67bc2dd9cea0bb1
km2457 <31201028+km2457@users.noreply.github.com>
2018-01-10 14:22:58 +0800

    Update ebridge_new.go

--- ebridge_new.go
+++ ebridge_new.go
@@ -37,7 +37,7 @@ func LongGet(pv string) (int, error) {
 	if ezcaReturn != C.EZCA_OK {
 		return -1, errors.New("long PV获取失败")
 	}
-	 time.Sleep(time.Second * 1)
+	 time.Tick(time.Second * 1)
 	return *result, nil
 }
 
