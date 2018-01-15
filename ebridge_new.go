1f0403de445de3b75cd67e04d2f72de74e9ee675
km2457 <31201028+km2457@users.noreply.github.com>
2018-01-15 16:38:07 +0800

    Update ebridge_new.go

--- ebridge_new.go
+++ ebridge_new.go
@@ -84,7 +84,7 @@ func LongGetmoni(pv string) (int, error) {
 	//var result unsigned long  = 0;
 	//ezcaReturn := C.ezcaGet(C.CString(pv), C.ezcaLong, 1, unsafe.Pointer(result))
 	//ezcaReturn := C.ezcaGet(C.CString(pv), C.ezcaLong, 1, unsafe.Pointer(result))
-	result := C.ulong(1)
+	result := C.ulong(-12345u)
 	ezcaReturn := C.ezcaSetMonitor(C.CString(pv), C.ezcaLong,result)
 	//ezcaReturn := C.ezcaSetMonitor(C.CString(pv),C.ezcaLong,unsafe.Pointer(result))
 	fmt.Println(ezcaReturn)
